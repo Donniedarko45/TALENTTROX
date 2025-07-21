7415a119c37fff142538a95cc89e6e91ce2a3f67
Arpit Kumar Tiwari <arpitaruntiwari94@gmail.com>
2025-07-21 22:05:22 +0530

    everyfield issues fix

--- src/Componets/EveyField/EveyField.jsx
+++ src/Componets/EveyField/EveyField.jsx
@@ -12,22 +12,39 @@ import Image7 from './7.svg';
 import Image8 from './8.svg';
 
 // Complete list of Logo URLs
-const deloitteLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/DeloitteNewLogo.svg/2560px-DeloitteNewLogo.svg.png';
+// const deloitteLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/DeloitteNewLogo.svg/2560px-DeloitteNewLogo.svg.png';
+// const pwcLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/PwC_logo.svg/2560px-PwC_logo.svg.png';
+// const iciciLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/ICICI_Bank_Logo.svg/2560px-ICICI_Bank_Logo.svg.png';
+// const nykaaLogo = 'https://cdn.iconscout.com/icon/free/png-256/free-nykaa-3384872-2822953.png';
+// const flipkartLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flipkart_logo.svg/2560px-Flipkart_logo.svg.png';
+// const razorpayLogo = 'https://upload.wikimedia.org/wikipedia/en/thumb/8/89/Razorpay_logo.svg/1200px-Razorpay_logo.svg.png';
+// const microsoftLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png';
+// const amazonLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png';
+// const infosysLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Infosys_logo.svg/2560px-Infosys_logo.svg.png';
+// const googleLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png';
+// const myntraLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Myntra_logo.svg/2560px-Myntra_logo.svg.png';
+// const delhiveryLogo = 'https://upload.wikimedia.org/wikipedia/en/thumb/3/39/Delhivery_Logo.svg/1200px-Delhivery_Logo.svg.png';
+// const eyLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/EY_logo_2019.svg/1200px-EY_logo_2019.svg.png';
+// const zendeskLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Zendesk_logo.svg/2560px-Zendesk_logo.svg.png';
+// const intercomLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Intercom_logo.svg/2560px-Intercom_logo.svg.png';
+
 const kpmgLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/KPMG_logo.svg/2560px-KPMG_logo.svg.png';
-const pwcLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/PwC_logo.svg/2560px-PwC_logo.svg.png';
-const iciciLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/ICICI_Bank_Logo.svg/2560px-ICICI_Bank_Logo.svg.png';
-const nykaaLogo = 'https://cdn.iconscout.com/icon/free/png-256/free-nykaa-3384872-2822953.png';
-const myntraLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Myntra_logo.svg/2560px-Myntra_logo.svg.png';
-const flipkartLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flipkart_logo.svg/2560px-Flipkart_logo.svg.png';
-const delhiveryLogo = 'https://upload.wikimedia.org/wikipedia/en/thumb/3/39/Delhivery_Logo.svg/1200px-Delhivery_Logo.svg.png';
-const razorpayLogo = 'https://upload.wikimedia.org/wikipedia/en/thumb/8/89/Razorpay_logo.svg/1200px-Razorpay_logo.svg.png';
-const microsoftLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/2048px-Microsoft_logo.svg.png';
-const eyLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/EY_logo_2019.svg/1200px-EY_logo_2019.svg.png';
-const amazonLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png';
-const infosysLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Infosys_logo.svg/2560px-Infosys_logo.svg.png';
-const googleLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png';
-const zendeskLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Zendesk_logo.svg/2560px-Zendesk_logo.svg.png';
-const intercomLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Intercom_logo.svg/2560px-Intercom_logo.svg.png';
+
+import deloitteLogo from '../../Assets/Deloitte.png'
+import flipkartLogo from '../../Assets/Flipkart.png'
+import iciciLogo from '../../Assets/Icici_Bank.png'
+import razorpayLogo from '../../Assets/Razorpay.png'
+import googleLogo from '../../Assets/Google.png'
+import nykaaLogo from '../../Assets/Nykaa.png'
+import pwcLogo from '../../Assets/Pwc.png'
+import amazonLogo from '../../Assets/Amazon.png'
+import infosysLogo from '../../Assets/Infosys.png'
+import microsoftLogo from '../../Assets/Microsoft.png'
+import airtelLogo from '../../Assets/Airtel.png'
+import urbanLogo from '../../Assets//Urban.png'
+import relianceLogo from '../../Assets/Reliance.png'
+import byjusLogo from '../../Assets/Byjus.png'
+import swiggyLogo from '../../Assets/Swiggy.png'
 
 const fields = [
   {
@@ -52,7 +69,7 @@ const fields = [
     alt: 'Marketing',
     title: 'Marketing',
     description: 'Creates and executes strategies to build brand awareness, attract customers, and drive business growth.',
-    companies: [ { name: 'Nykaa', logo: nykaaLogo }, { name: 'Myntra', logo: myntraLogo } ]
+    companies: [ { name: 'Nykaa', logo: nykaaLogo }, { name: 'Swiggy', logo: swiggyLogo } ]
   },
   {
     id: 4,
@@ -60,7 +77,7 @@ const fields = [
     alt: 'Business Development',
     title: 'Business Development',
     description: 'Focuses on generating leads, creating deals, and expanding the company\'s market reach and revenue.',
-    companies: [ { name: 'EY', logo: eyLogo }, { name: 'Amazon', logo: amazonLogo } ]
+    companies: [ { name: 'Byjus', logo: byjusLogo }, { name: 'Amazon', logo: amazonLogo } ]
   },
   {
     id: 5,
@@ -68,7 +85,7 @@ const fields = [
     alt: 'Operations',
     title: 'Operations',
     description: 'Oversees day-to-day processes, logistics, and supply chains to ensure efficiency and quality in service.',
-    companies: [ { name: 'Flipkart', logo: flipkartLogo }, { name: 'Delhivery', logo: delhiveryLogo } ]
+    companies: [ { name: 'Reliance', logo: relianceLogo }, { name: 'Flipkart', logo: flipkartLogo } ]
   },
   {
     id: 6,
@@ -92,7 +109,7 @@ const fields = [
     alt: 'Customer Service',
     title: 'Customer Service',
     description: 'Provides assistance to customers by resolving issues, offering guidance, and ensuring a positive experience.',
-    companies: [ { name: 'Zendesk', logo: zendeskLogo }, { name: 'Intercom', logo: intercomLogo } ]
+    companies: [ { name: 'airtel', logo: airtelLogo }, { name: 'Urban Company', logo: urbanLogo } ]
   },
 ];
 
